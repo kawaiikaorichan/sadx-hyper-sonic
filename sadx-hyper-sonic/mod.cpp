6d4067400181d054a40bcff7af9a98f77028bf09
LivelyZephyrs <electriostarr@gmail.com>
2021-05-02 16:39:38 -0400

    WIP of further ingame voice replacements

--- sadx-hyper-sonic/mod.cpp
+++ sadx-hyper-sonic/mod.cpp
@@ -16,9 +16,18 @@ static ObjectMaster* InvincibilityObject[max_players]{};
 void __cdecl PlayVoice_r(int voice);
 Trampoline PlayVoice_t((int)PlayVoice, (int)PlayVoice + 0x5, PlayVoice_r);
 void __cdecl PlayVoice_r(int voice) {
-	if (CountEmblems(&SaveFile) >= 130 && (Rings >= 100 || GameState != 15)) {
-		if (voice == 396) { // Replace Sonic transformation grunt if Hyper Sonic
-			voice = 9500;
+	// If about to transform into Hyper Sonic OR in menu, replace the grunt voice
+	if (CountEmblems(&SaveFile) >= 130 && (Rings >= 100 || GameState != 15) && voice == 396) {
+		voice = 9500;
+	}
+
+	// If Hyper Sonic
+	if (HyperSonic[0] == true) {
+		switch (voice) {
+		case 1849: voice = 9501; break; // 1UP, INV
+		case 1850: voice = 9502; break; // Speed shoes
+		case 388: voice = 9503; break; // Unused Super(?) Sonic dialogue
+		case 1843: voice = 9504; break; // Sonic boss defeat dialogue
 		}
 	}
 
